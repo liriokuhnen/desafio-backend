a73894e9216495cb779303779a3da7aa91ac8a8e
Lirio Kuhnen <lirio_kuhnen@hotmail.com>
2020-05-15 03:45:12 -0300

    create model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Classification(models.Model):
+    classification = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.classification
+
+class Travel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    classification = models.ForeignKey(Classification, blank=True, null=True, on_delete=models.SET_NULL)
+    start_date = models.DateTimeField(default=datetime.now)
+    finish_date = models.DateTimeField(default=datetime.now)
+    evaluate = models.IntegerField(blank=True, null=True, validators=[
+        MaxValueValidator(5),
+        MinValueValidator(1)
+    ])
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.id, self.user.username, self.classification)
