504fb95e470196a78421d541df3c80fcf513a306
Lirio Kuhnen <lirio_kuhnen@hotmail.com>
2020-05-15 03:48:02 -0300

    configuration by environment

--- manage.py
+++ manage.py
@@ -5,7 +5,17 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tbe.settings')
+
+    env = os.environ.get('ENV') or 'local'
+
+    mapped_env = {
+        'prod': 'tbe.settings.prod',
+        'stage': 'tbe.settings.stage',
+        'local': 'tbe.settings.local',
+        'docker': 'tbe.settings.docker',
+    }
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', mapped_env[env])
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
